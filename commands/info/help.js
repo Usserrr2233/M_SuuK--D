ab7c655b41dc457fd095b39594ac3e6309d02d41
Usserrr2233 <163590768+Usserrr2233@users.noreply.github.com>
2024-03-25 08:29:22 +0000

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -18,25 +18,25 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
             /*
                             .addFields(
-                                { name: `${prefix}247`, value: `Toggles the 24/7 mode. This makes the bot doesn't leave the voice channel until you stop it.`, inline: false },
-                                { name: `${prefix}autoplay`, value: `Toggles autoplay for the current guild.`, inline: false },
-                                { name: `${prefix}join`, value: `join the voice channel.`, inline: false },
-                                { name: `${prefix}jump`, value: `Skip to a song in the queue.`, inline: false },
-                                { name: `${prefix}leave`, value: `leave the voice channel.`, inline: false },
-                                { name: `${prefix}loop`, value: `Toggles the repeat mode.`, inline: false },
-                                { name: `${prefix}lyrics`, value: `Display lyrics of a song`, inline: false },
-                                { name: `${prefix}nowplaying`, value: `Shows what is song that the bot is currently playing.`, inline: false },
-                                { name: `${prefix}pause`, value: `Pauses the currently playing track.`, inline: false },
-                                { name: `${prefix}play`, value: `Add a song to queue and plays it.`, inline: false },
-                                { name: `${prefix}previous`, value: `Plays the previous song in the queue.`, inline: false },
-                                { name: `${prefix}queue`, value: `Display the queue of the current tracks in the playlist.`, inline: false },
-                                { name: `${prefix}resume`, value: `Resumes the currently paused track.`, inline: false },
-                                { name: `${prefix}search`, value: `Search song and play music.`, inline: false },
-                                { name: `${prefix}seek`, value: `Seeks to a certain point in the current track.`, inline: false },
-                                { name: `${prefix}shuffle`, value: `Shuffle the queue.`, inline: false },
-                                { name: `${prefix}skip`, value: `Skip the current song.`, inline: false },
-                                { name: `${prefix}stop`, value: `Stop the current song and clears the entire music queue.`, inline: false },
-                                { name: `${prefix}volume`, value: `Changes/Shows the current volume.`, inline: false },
+                                { name: `${prefix}247`, value: `➢ Toggles the 24/7 mode. This makes the bot doesn't leave the voice channel until you stop it.`, inline: false },
+                                { name: `${prefix}autoplay`, value: `➢ Toggles autoplay for the current guild.`, inline: false },
+                                { name: `${prefix}join`, value: `➢ join the voice channel.`, inline: false },
+                                { name: `${prefix}jump`, value: `➢ Skip to a song in the queue.`, inline: false },
+                                { name: `${prefix}leave`, value: `➢ leave the voice channel.`, inline: false },
+                                { name: `${prefix}loop`, value: `➢ Toggles the repeat mode.`, inline: false },
+                                { name: `${prefix}lyrics`, value: `➢ Display lyrics of a song`, inline: false },
+                                { name: `${prefix}nowplaying`, value: `➢ Shows what is song that the bot is currently playing.`, inline: false },
+                                { name: `${prefix}pause`, value: `➢ Pauses the currently playing track.`, inline: false },
+                                { name: `${prefix}play`, value: `➢ Add a song to queue and plays it.`, inline: false },
+                                { name: `${prefix}previous`, value: `➢ Plays the previous song in the queue.`, inline: false },
+                                { name: `${prefix}queue`, value: `➢ Display the queue of the current tracks in the playlist.`, inline: false },
+                                { name: `${prefix}resume`, value: `➢ Resumes the currently paused track.`, inline: false },
+                                { name: `${prefix}search`, value: `➢ Search song and play music.`, inline: false },
+                                { name: `${prefix}seek`, value: `➢ Seeks to a certain point in the current track.`, inline: false },
+                                { name: `${prefix}shuffle`, value: `➢ Shuffle the queue.`, inline: false },
+                                { name: `${prefix}skip`, value: `➢ Skip the current song.`, inline: false },
+                                { name: `${prefix}stop`, value: `➢ Stop the current song and clears the entire music queue.`, inline: false },
+                                { name: `${prefix}volume`, value: `➢ Changes/Shows the current volume.`, inline: false },
                                 )
             */
             commandFiles.map((value) => {
@@ -60,8 +60,8 @@ module.exports = {
                 .addComponents(
                     new ButtonBuilder()
                         .setStyle(ButtonStyle.Link)
-                        .setLabel('Server Support')
-                        .setURL(`https://discord.gg/developer-tools`))
+                        .setLabel('Playlist Spotify')
+                        .setURL(`https://open.spotify.com/playlist/37i9dQZF1DXb7WmotStdsj`))
 
             message.reply({ embeds: [embed], components: [row] })
         } catch (err) {
